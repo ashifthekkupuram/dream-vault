7ed2c735fb848dee735014f2177d27dab86d7e79
Ashi <ashi@debian.wifi>
2026-01-20 22:36:57 +0530

    in the backend add origins in better auth to fix cors

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -3,8 +3,14 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db';
 
+if (!process.env.FRONTEND_URL) {
+  throw new Error('FRONTEND_URL required in the .env');
+}
+
 if (!process.env.BETTER_AUTH_SECRET || !process.env.BETTER_AUTH_URL) {
-  throw new Error('Better Auth Secret and URL required');
+  throw new Error(
+    'BETTER_AUTH_SECRET and BETTER_AUTH_URL required in the .env',
+  );
 }
 
 export const auth = betterAuth({
@@ -14,4 +20,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: [process.env.FRONTEND_URL],
 });
