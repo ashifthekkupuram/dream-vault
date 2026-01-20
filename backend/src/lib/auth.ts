dcac502f045406b80ea7c347bf4bb6c994f918f3
Ashi <ashi@debian.wifi>
2026-01-20 22:52:35 +0530

    in the backend added schema in drizzle adpater better-auth config to fix issues

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db';
+import { userTable, sessionTable, verificationTable, accountTable } from '../db/schema';
 
 if (!process.env.FRONTEND_URL) {
   throw new Error('FRONTEND_URL required in the .env');
@@ -16,6 +17,12 @@ if (!process.env.BETTER_AUTH_SECRET || !process.env.BETTER_AUTH_URL) {
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema: {
+      user: userTable,
+      account: accountTable,
+      session: sessionTable,
+      verification: verificationTable
+    }
   }),
   emailAndPassword: {
     enabled: true,
